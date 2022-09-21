6a0053d29376fd8290310ffa11c16d0410649123
Serj Zzz <s.zavolzhskiy@gmail.com>
2022-09-22 00:54:41 +0400

    complete task Arrays

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -2,11 +2,27 @@ module Exercise
   module Arrays
     class << self
       def replace(array)
-        array
+        max_el = array[0]
+        array.each { |el| max_el = el if max_el < el }
+        array.map { |el| el.positive? ? max_el : el }
       end
 
       def search(_array, _query)
-        0
+        return -1 if _array.empty?
+        return 0 if _array.first == _query
+
+        recursive_search(_array, _query, 0, _array.size - 1)
+      end
+
+      private
+
+      def recursive_search(arr, target, low, high)
+        return -1 if low > high
+
+        mid = (low + high) / 2
+        return mid if target == arr[mid]
+
+        target < arr[mid] ? recursive_search(arr, target, low, mid - 1) : recursive_search(arr, target, mid + 1, high)
       end
     end
   end
