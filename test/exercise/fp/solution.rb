20fdd694d87d15c52659b966b9946eba088cd350
Serj Zzz <s.zavolzhskiy@gmail.com>
2022-09-22 21:48:06 +0400

    complete task 1 - rating mean

--- test/exercise/fp/solution.rb
+++ test/exercise/fp/solution.rb
@@ -4,8 +4,12 @@ module Exercise
       # Обратиться к параметрам фильма можно так:
       # film["name"], film["rating_kinopoisk"], film["rating_imdb"],
       # film["genres"], film["year"], film["access_level"], film["country"]
-      def rating(_array)
-        0
+      def rating(array)
+        filter_films = array.map do |film|
+           film['rating_kinopoisk'].to_f if !film['country'].nil? && film['country'].rstrip.split(',').size > 1 && film['rating_kinopoisk'].to_f.positive?
+        end.compact
+        
+        filter_films.reduce(0) { |sum, value| sum + value } / filter_films.size
       end
 
       def chars_count(films, threshold)
