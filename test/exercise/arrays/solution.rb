d0abf682585fdfe626b4dd111e69d6a4aeee4a12
Serj Zzz <s.zavolzhskiy@gmail.com>
2022-09-22 13:31:47 +0400

    refactor search method

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -7,22 +7,13 @@ module Exercise
         array.map { |el| el.positive? ? max_el : el }
       end
 
-      def search(_array, _query)
-        return -1 if _array.empty?
-        return 0 if _array.first == _query
-
-        recursive_search(_array, _query, 0, _array.size - 1)
-      end
-
-      private
-
-      def recursive_search(arr, target, low, high)
-        return -1 if low > high
+      def search(array, query, low = 0, high = array.size - 1)
+        return -1 if (low > high) || array.empty?
 
         mid = (low + high) / 2
-        return mid if target == arr[mid]
+        return mid if query == array[mid]
 
-        target < arr[mid] ? recursive_search(arr, target, low, mid - 1) : recursive_search(arr, target, mid + 1, high)
+        query < array[mid] ? search(array, query, low, mid - 1) : search(array, query, mid + 1, high)
       end
     end
   end
