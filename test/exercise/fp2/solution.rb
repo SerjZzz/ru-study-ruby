c141198c60c5a36df208337d41b65cf09fde1e84
Serj Zzz <s.zavolzhskiy@gmail.com>
2022-09-23 17:42:32 +0400

    develop each, reduce, map, compact methods

--- test/exercise/fp2/solution.rb
+++ test/exercise/fp2/solution.rb
@@ -5,16 +5,29 @@ module Exercise
       # Использовать свои написанные функции для реализации следующих - можно.
 
       # Написать свою функцию my_each
-      def my_each; end
+      def my_each(&block)
+        first, *other = self
+        block.call(first)
+        other.size.zero? ? [first] : [first, *MyArray.new(other).my_each(&block)]
+      end
 
       # Написать свою функцию my_map
-      def my_map; end
+      def my_map(&block)
+        MyArray.new(my_reduce([]) { |acc, el| acc << block.call(el) })
+      end
 
       # Написать свою функцию my_compact
-      def my_compact; end
+      def my_compact
+        MyArray.new(my_reduce([]) { |accumulator, element| element.nil? ? accumulator : accumulator << element })
+      end
 
       # Написать свою функцию my_reduce
-      def my_reduce; end
+      def my_reduce(accumulator = nil, &block)
+        return accumulator if size.zero?
+
+        accumulator = accumulator.nil? ? first : block.call(accumulator, first)
+        MyArray.new(self[1..size]).my_reduce(accumulator, &block)
+      end
     end
   end
 end
